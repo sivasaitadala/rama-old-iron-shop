9dc8236e6b7a36c465ea0079741a0effb25266bc
sivasaitadala <sivasaitadala@gmail.com>
2026-04-16 10:42:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("RAMA Old Iron Shop Website Loaded");
+const toggleBtn = document.getElementById("theme-toggle");
+
+toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggleBtn.textContent =
+        document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
