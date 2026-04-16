d4421a68961abb61f64ae18d53330cafd8086449
sivasaitadala <sivasaitadala@gmail.com>
2026-04-16 14:51:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.getElementById("theme-toggle");
-
-toggleBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-
-    toggleBtn.textContent =
-        document.body.classList.contains("dark") ? "☀️" : "🌙";
-});
